c012b39358380682af7d9fe7a231f82b41ce96e6
Nick Jardine <nickjar86@gmail.com>
2025-01-05 18:22:56 -0700

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Next.js image gallery",
+  description: "Next Image Gallery",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <main className="max-w-6xl mx-auto">{children}</main>
+      </body>
+    </html>
+  );
+}
